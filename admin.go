2208858400295ee336bccbe97ddd245529f17633
Dustin Sallings <dustin@spy.net>
2016-01-20 08:22:27 -0800

    Only grab the most recent entries for updating tune results

--- admin.go
+++ admin.go
@@ -20,7 +20,7 @@ func init() {
 func handleRewriteUUIDs(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
-	q := datastore.NewQuery("TuneResults")
+	q := datastore.NewQuery("TuneResults").Order("-timestamp").Limit(50)
 	res := []TuneResults{}
 	if err := fillKeyQuery(c, q, &res); err != nil {
 		log.Errorf(c, "Error fetching tune results: %v", err)
